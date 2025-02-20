a748a39df743e5e23dbe83ffc64fc72cf769d034
KennedyMen <kennedy.mensah@bison.howard.edu>
2025-02-20 12:10:21 -0500

    added requirements.txt

--- creds.py
+++
@@ -1,8 +0,0 @@
-import random
-
-host = "brd.superproxy.io"
-port = 33335
-username ="brd-customer-hl_5abbed84-zone-datacenter_proxy1"
-password = "k54fqfrc20li"
-session_id = random.random()
-proxy_url=('http://{}-session-{}:{}@{}:{}'.format(username, session_id ,password,host, port))
